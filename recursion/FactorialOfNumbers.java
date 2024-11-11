da6e59dde4d666c9b32550273729e8af28d64df5
wasif6793@gmail.com <wasif@example.com>
2024-11-11 15:36:01 +0530

    Recursion

--- recursion/FactorialOfNumbers.java
+++ recursion/FactorialOfNumbers.java
@@ -1,4 +1,15 @@
 package recursion;
-
+// Finding Factorial of a number using recursion
 public class FactorialOfNumbers {
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(factorial(n));
+    }
+
+    private static int factorial(int n) {
+
+        if(n == 1) return 1;
+
+        return n * factorial(n - 1);
+    }
 }
