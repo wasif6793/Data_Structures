c4b80584c86cc1e8308ad683a17b91b2d8da46a4
wasif6793@gmail.com <wasif@example.com>
2024-11-12 10:18:34 +0530

    Bubble Sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -2,6 +2,24 @@ package Sorting;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        System.out.println("nill");
+
+        int nums[] = {2,5,4,6,1,3};
+        bubbleSort(nums);
+        for(int num : nums){
+            System.out.print(num+ " ");
+        }
+    }
+
+    private static void bubbleSort(int[] nums) {
+
+        for (int i = 0; i <nums.length; i++) {
+            for (int j = 0; j < nums.length - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
     }
 }
